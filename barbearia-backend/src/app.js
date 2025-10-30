a923f904446e29ff05bb8881e51ea2929b26954d
Julio <bevilacquajulio@outlook.com>
2025-10-30 10:39:38 -0300

    concerto api

--- barbearia-backend/src/app.js
+++ barbearia-backend/src/app.js
@@ -3,15 +3,6 @@ import express from 'express'
 import cors from 'cors'
 import { adicionarRotas } from './rotas.js'
 
-
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Configurar a pasta uploads como pública
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
 const api = express()
 api.use(cors())
 api.use(express.json())
@@ -20,4 +11,3 @@ adicionarRotas(api)
 
 const PORT = process.env.PORT || 3000
 api.listen(PORT, () => console.log(`--> API Barbearia subiu na porta ${PORT} <--`))
-
